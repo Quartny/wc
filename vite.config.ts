5419cd0f80cfc33440a4128f5a0136c99315a932
Qᴜᴀʀᴛɴʏ <142509341+Quartny@users.noreply.github.com>
2026-09-14 13:04:00 +0000

    Added!

--- vite.config.ts
+++
@@ -1,6 +0,0 @@
-import { defineConfig } from 'vite';
-
-export default defineConfig(() => ({
-  server: { headers: { 'Cache-Control': 'no-store' } },
-  preview: { headers: { 'Cache-Control': 'no-store' } },
-}));
